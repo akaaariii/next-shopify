a98c7ea787f5ff348b30ee644c18d39cbc9ae7f7
akaaariii <caaari23@gmail.com>
2021-06-30 16:51:55 -0700

    token

--- shopify/shopify.js
+++ shopify/shopify.js
@@ -3,7 +3,7 @@ import Client from "shopify-buy";
 const client = Client.buildClient({
   domain: "home-appliances-ca.myshopify.com",
   // Storefront API access tokens are not secret. You can place them in a JavaScript file or any public HTML document.
-  storefrontAccessToken: "363669dc4fa52deb236c332d5427538f",
+  storefrontAccessToken: process.env.NEXT_PUBLIC_STOREFRONT_ACCESS_TOKEN,
 });
 
 export default client;
